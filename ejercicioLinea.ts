d792f0f29feac5bc3c73dd17ddb9666848b777f9
Fer-efe <ferpolanco83@gmail.com>
2024-05-13 10:18:34 -0300

    Terminando funcion línea

--- ejercicioLinea.ts
+++ ejercicioLinea.ts
@@ -1,8 +1,15 @@
-let linea:string =" ";
 
-for(let i:number=0; i<= 40; i++){
-    linea= linea + "-"
+function dibujarLinea(){
+    let linea:string =" ";
+
+    for(let i:number=0; i<= 40; i++){
+        linea= linea + "-"
+    }
+    console.log(linea);
+
 }
-console.log(linea);
+
+dibujarLinea();
+
 
 
